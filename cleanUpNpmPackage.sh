d20230bd54cbe3da75a36bc5226775332d648823
BJ McCotter <bj.mccotter@adhocteam.us>
2022-04-19 14:48:48 -0500

    Adding a cleanup script for node/npm packages

---
+++ cleanUpNpmPackage.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+NODE_MODULES=$PWD/node_modules
+DIST=$PWD/dist
+BUILD=$PWD/build
+
+if [ -d $NODE_MODULES ]; then
+	echo "Removing node_modules, please wait this could take some time........"
+	rm -rf node_modules
+fi
+
+if [ -d $DIST ]; then
+	echo "Removing dist directory, please wait this could take some time........"
+	rm -rf dist
+fi
+
+if [ -d $BUILD ]; then
+	echo "Removing build directory, please wait this could take some time........"
+	rm -rf build
+fi
+
+echo "Finished removing all directories, please run your npm/yarn install again!"
